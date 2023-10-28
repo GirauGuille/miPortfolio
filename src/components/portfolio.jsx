b0036cf896ae2407ab6cc7d438f88e3a0c923e72
Guillermo Girau <girauguillermo@gmail.com>
2023-10-28 07:44:53 -0300

    avanzando

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,5 +1,9 @@
 import React, { Fragment, useState } from 'react'
 import education from '../data/education.json'
+/* import education from '../data/skills.json' */
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 function portfolio() {
     const [activeSection, setActiveSection] = useState('education');
@@ -9,7 +13,7 @@ function portfolio() {
     };
 
     return (
-        <div className='portfolio' id='portfolio'>
+        <section className='portfolio' id='portfolio'>
             <h2 className='cTitle'>PORTFOLIO</h2>
             <nav>
                 <ul>
@@ -21,7 +25,7 @@ function portfolio() {
                     <li
                         className={activeSection === 'skills' ? 'active' : ''}
                         onClick={() => handleSectionChange('skills')}>
-                        Skills
+                        Habilidades
                     </li>
                 </ul>
             </nav>
@@ -52,10 +56,17 @@ function portfolio() {
                 {activeSection === 'skills' && (
                     <div>
                         <h2>SKILLS</h2>
+                        <div className='progress__box'>
+                            <div className="progress__circle">
+                                <CircularProgressbar strokeWidth={3}/>
+                            </div>
+
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" fillRule="evenodd" d="m1.5 0l1.547 18l6.943 2l6.961-2.002L18.5 0h-17Zm13.773 4.272l-.097 1.119l-.043.496H6.99l.195 2.26h7.755l-.052.594l-.5 5.812l-.032.373L10 16.178l-.01.004l-4.36-1.256l-.297-3.467h2.136l.151 1.762l2.37.663h.002l2.374-.665l.247-2.863H5.237l-.523-6.084l-.05-.593h10.66l-.051.593Z"/></svg>
                     </div>
                 )}
             </div>
-        </div>
+        </section>
     )
 }
 
